540f9dc64411f3d4a5e48b0596e82b00131d0531
Narottam <coutkunal@gmail.com>
2025-07-25 13:26:58 +0530

    added correct sitemap and robots.txt configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,11 +9,24 @@ import sitemap from "@astrojs/sitemap";
 
 import playformCompress from "@playform/compress";
 
+import robotsTxt from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://webdrip.in",
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [icon(), sitemap(), playformCompress()],
+  integrations: [
+    robotsTxt({
+      sitemap: [
+        "https://webdrip.in/sitemap-index.xml",
+        "http://webdrip.in/sitemap-0.xml",
+      ],
+    }),
+    icon(),
+    sitemap(),
+    playformCompress(),
+  ],
 });
